6dfc5624864b1b4cbd2cd42b12bcadb392c365a0
efensyjn <me@efensyjn.moe>
2025-08-26 16:03:09 +0500

    ollama

--- hosts/waguri/default.nix
+++ hosts/waguri/default.nix
@@ -30,5 +30,19 @@
 
   mods.plasma.enable = true;
 
+  services.mongodb = {
+    enable = true;
+  };
+
+  services.ollama = {
+    enable = true;
+    loadModels = [ "deepseek-r1" "gemma3" "llama3.1" "llama2-uncensored" ];
+    acceleration = "cuda";
+  };
+  services.open-webui = {
+    enable = true;
+    openFirewall = true;
+  };
+
   system.stateVersion = "25.05";
 }
