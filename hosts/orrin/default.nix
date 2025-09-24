f484652d60a37a2e311e739f75832d40a962211e
efensyjn <me@efensyjn.moe>
2025-09-24 06:41:11 +0500

    orrin: cloudflared

--- hosts/orrin/default.nix
+++ hosts/orrin/default.nix
@@ -5,6 +5,9 @@
     ../../mods
   ];
 
+  sops.defaultSopsFile = ../../secrets/efe.yaml;
+  sops.age.keyFile = "/var/lib/sops-nix/ghe.txt";
+
   services.automatic-timezoned.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -42,7 +45,15 @@
     enable = true;
   };
 
-  systemd.services.gitlab-backup.environment.BACKUP = "dump";
+  services.cloudflared = {
+    enable = true;
+#    tunnels = {
+#      "35752cf2-0cf4-49c2-8470-00b7e3b39b45" = {
+#        credentialsFile = "${config.sops.secrets.cloudflared-creds.path}";
+#        default = "http_status:404";
+#      };
+#    };
+  };
 
   system.stateVersion = "25.05";
 }
